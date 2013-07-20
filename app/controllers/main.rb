5fccedc0db314211efc97532b8bf8980d69899c5
Katherine Whitlock <toroidalcode@gmail.com>
2013-07-19 22:03:30 -0400

    Removed oauth and simplified

--- app/controllers/main.rb
+++ app/controllers/main.rb
@@ -1,21 +1,7 @@
 Licensor::App.controllers do
   configure do
-    options = {
-      site: 'https://github.com/',
-      authorize_url: '/login/oauth/authorize',
-      token_url: '/login/oauth/access_token',
-    }
-      
-    set :client, OAuth2::Client.new(ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], options)
-  end
-  
-  get '/authorize' do
-    params = {
-      scope: 'public_repo',
-    }  
-    
-    # Redirect the user to the authorize_uri endpoint
-    redirect settings.client.auth_code.authorize_url(params)
+    Octokit.client_id = ENV['CLIENT_ID']
+    Octokit.client_secret = ENV['CLIENT_SECRET']
   end
 
   get "/" do
--- app/controllers/repos.rb
+++ app/controllers/repos.rb
@@ -1,16 +1,8 @@
 Licensor::App.controllers :repos do
 
   get :index do
-    # Pull the authorization_code out of the response
-    code = params[:code]
-    
-    # Make a request to the access_token_uri endpoint to get an access_token
-    access_token = settings.client.auth_code.get_token(code, params: {scope: 'public_repo'}).token
-    @client = Octokit::Client.new(login: 'me', oauth_token: access_token)    
-    # Use the access_token as you would a regular live-mode API key
     render '/repos/index'
   end
-
 
   # get :index, :map => '/foo/bar' do
   #   session[:foo] = 'bar'
