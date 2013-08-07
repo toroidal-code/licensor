23169f0acccad1a9a9b095fec7786bc8d1331454
Nicolas McCurdy <thenickperson@gmail.com>
2013-08-07 19:43:26 -0400

    Extract some new helper methods out of the views

--- app/helpers/repos_helper.rb
+++ app/helpers/repos_helper.rb
@@ -1,7 +1,19 @@
 # Helper methods defined here can be accessed in any controller or view in the application
 
 Licensor::App.helpers do
-  # def simple_helper_method
-  #  ...
-  # end
+  def is_licensed?(repo)
+    Octokit.contents(repo).any? { |file| /license/i =~ file['name'] }
+  end
+
+  def page_title
+    [@title, 'Licensor'].compact.join ' | '
+  end
+
+  def repository_list
+    repositories = Octokit.repos params[:username], type: 'owner', sort: 'pushed'
+    repository_names = repositories.map { |repo| repo['full_name'] }
+    partial 'repos/repo', collection: repository_names
+  rescue
+    redirect 'errors/nouser'
+  end
 end
